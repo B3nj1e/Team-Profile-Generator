14cff33d2f28ae46d4c027b6711691a864f5b076
Benjamin Clewer <benjiclewer@gmail.com>
2021-11-12 11:06:18 +1030

    inquirer questions now working, half written

--- src/questions.js
+++ src/questions.js
@@ -1,39 +1,127 @@
 const inquirer = require('inquirer');
-const Manager = require("../lib/Manager")
+
+
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            message: "What is the name of your team Manager?",
+            name: "manager",
+        },
+        {
+            type: 'number',
+            message: "What is their employee ID?",
+            name: "id",
+        },
+        {
+            type: 'input',
+            message: "What is their email address?",
+            name: "email"
+        },
+        {
+            type: 'number',
+            message: "What is their office number?",
+            name: "officenumber"
+        },
+        {
+            type: 'confirm',
+            message: "Would you like to add another team member",
+            name: "anotherone",
+        }])
+    .then((data) => {
+        if (data.anotherone) {
+            console.log(data);
+            inquirer.prompt(
+                {
+                    type: 'list',
+                    message: "What is the position of your team member?",
+                    name: "position",
+                    choices: ["Engineer", "Intern"]
+                })}}
+            ).then((data) => {
+                if (data.position == "Engineer") {
+                    console.log(data);
+                    inquirer.prompt([
+                        {
+                            type: 'input',
+                            message: "What is their name?",
+                            name: "name",
+                        },
+                        {
+                            type: 'number',
+                            message: "What is their employee ID?",
+                            name: "id",
+                        },
+                        {
+                            type: 'input',
+                            message: "What is their email address?",
+                            name: "email"
+                        },
+                        {
+                            type: 'input',
+                            message: "What is their GitHub username?",
+                            name: "GitHub"
+                        },
+                        {
+                            type: 'confirm',
+                            message: "Would you like to add another team member",
+                            name: "anotherone",
+                        }])}}
+                        ).then((data) => {
+                        if (data.anotherone) {
+                            console.log(data);
+                            // question()
+                        } else {
+                            return data;
+                        }});
+                  
+
+
+function question() {
+    inquirer.prompt([
+        {
+            type: 'list',
+            message: "What is the position of your team member?",
+            name: "position",
+            choices: ["Engineer", "Intern"]
+        },
+        {
+            type: 'input',
+            message: "What is their name?",
+            name: "name",
+        },
+    ])
+};
 
 
 
-inquirer   
-.prompt([
-    {
-        type: 'list', 
-        message: "What is the position of your team member?",
-        name: "position",
-        choices: ["Manager", "Engineer", "Intern"]
-    },
-    {
-        type: 'input', 
-        message: "What is their name?",
-        name: "name",
-    },
-    {
-        type: 'input', 
-        message: "What is their employee ID?",
-        name: "id",
-    },
-    {
-        type: 'input', 
-        message: "What is their email address?",
-        name: "email"
-    }
-    ]).then((data) => {
-        if(data.postion == "Manager") {
-            const person1 = new Manager(data.name, data.id, data.email, data.position);
-            person1.getOfficeNumber();
-            console.log(`The Manager has the following details ${person1}`);
-        // } else if(data.position == "Employee") {
-        // const person1 = new Employee(data.name, data.id, data.email);
-        // console.log(`The Employee has the following details ${person1}`);}
-        console.log(data);
-        }}).catch((error) => console.log(error)
-    );
+
+
+    // ]).then((data) => {
+    //     if(data.postion == "Manager") {
+    //         const person1 = new Manager(data.name, data.id, data.email, data.position);
+    //         person1.getOfficeNumber();
+    //         console.log(`The Manager has the following details ${person1}`);
+    //     // } else if(data.position == "Employee") {
+    //     // const person1 = new Employee(data.name, data.id, data.email);
+    //     // console.log(`The Employee has the following details ${person1}`);}
+    //     console.log(data);
+    //     }}).catch((error) => console.log(error)
+    // );
+
+
+
+
+
+    // {
+    //     type: 'list', 
+    //     message: "What is the position of your team member?",
+    //     name: "position",
+    //     choices: ["Manager", "Engineer", "Intern"]
+    // },
+
+    // {
+    //     type: 'input', 
+    //     message: "What is their name?",
+    //     name: "name",
+    // },
